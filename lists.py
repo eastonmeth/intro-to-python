c6e5de2ce80788e44e9916eb8269f366b1d9b6ac
Easton Meth <easton.meth@gmail.com>
2024-10-15 14:31:07 -0400

    Create lists.py

---
+++ lists.py
@@ -0,0 +1,17 @@
+names = ['Playboi Carti', 'Kanye West', 'Bob', 'Ken Carson', 'Glorilla']
+names[2] = 'Rob'
+
+print(names[0:3])
+
+numbers = [1, 2, 3, 4, 5]
+numbers.append(6)
+
+print(numbers)
+print(1 in numbers)
+print(len(numbers))
+
+numbers.insert(0, -1)
+print(numbers)
+
+numbers.clear()
+print(numbers)
